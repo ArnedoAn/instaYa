b485c2bdcf81b62d1b3f7cae22145e3a8c6e6c7a
Andres Arnedo <andresarnedo2002@gmail.com>
2022-12-03 00:20:16 -0500

    cors use

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 require("dotenv").config();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const swaggerUI = require("swagger-ui-express"),
   swaggerDoc = require("../swagger.json");
@@ -21,6 +22,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 // Middleware setup
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
