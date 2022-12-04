ba06a91cde4cd056dbbb1be8b4de8de34fd9f9b1
Andres Arnedo <andresarnedo2002@gmail.com>
2022-12-04 17:41:52 -0500

    token test

--- src/routes/sendMail.js
+++ src/routes/sendMail.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get("/", Auth, async (req, res, next) => {
   try {
     const name = req.header("name");
+    console.log(req.header("token"));
     await updateStatus(name);
     const mails = await mailSchema.find({ "fromUser.name": name });
     res.status(200).json(mails);
